e7f7396c5860c4ebcb0784931a00b3763d3da75d
Adrien Chesneau <achesne@a-018818.ups.u-psud.fr>
2019-02-13 16:35:52 +0100

    Debut mesure de Cluster

--- src/Instance.cpp
+++ src/Instance.cpp
@@ -7,16 +7,16 @@ using namespace std;
 
 class Instance {
 
-    Instance::Instance(){
-        
-    }
+    Instance::Instance(std::vector<Point> v): pareto(v){ }
 
-    float cost_median(int i, int iprime){
-        float min = 0;
+    Point Instance::getPoint(int i){ return this->pareto[i]}
+
+    float cost_medoids(int i, int iprime){
+        float min = FLOAT_MAX;
         for(int I = i; I < iprime; I++){
             float sum = 0;
             for (int j = i; j < iprime; j++){
-                float sum += dist(this.get(j), this.get(I));
+                sum += this->getPoint(I).distance(this->getPoint(j));
             }
             if (sum < min){
                 min = sum;
@@ -25,5 +25,43 @@ class Instance {
         return min;
     }
 
+    float cost_means(int i, int iprime){
+        float sum = 0;
+        int cpt = 0;
+        float x = 0 ;
+        float y = 0;
+        for(int j= i; j < iprime; I++){
+                x += this->getPoint(j).getX();
+                y += this->getPoint(j).getY();
+                cpt += 1;
+        }
+        Point p(1/(iprime-i+1)*(x/cpt),1/(iprime-i+1)*(y/cpt));
+        for(int I = i; I < iprime; I++){
+            sum+= this->getPoint(I).distance(p);
+        }
+        return sum;
+    }
+
+    float cost_median(int i, int iprime){
+        float min = FLOAT_MAX;
+        for(int I = i; I < iprime; I++){
+            float sum = 0;
+            for (int j = i; j < iprime; j++){
+                float sum += this->getPoint(I).distance(this->getPoint(j));
+            }
+            sum = sqrt(sum);
+            if (sum < min){
+                min = sum;
+            }
+        }
+        return min;
+    }
     
+    float cost_dcenter(int i, int iprime){
+        
+    }
+
+    float cost_ccenter(int i, int iprime){
+        return 0.5*this->getPoint(i).distance(this->getPoint(iprime));
+    }
 };
--- src/Point.cpp
+++ src/Point.cpp
@@ -1,5 +1,6 @@
 #include "Point.h"
 #include <iostream>
+#include <cmath>
 using namespace std;
    
 Point::Point() : x(0), y(0) { }
@@ -7,4 +8,15 @@ Point::Point(float abs, float ord) : x(abs), y(ord) { }
 
 void Point::display(){
     cout << "x:" << x << ", y:" << y << "\n";
+}
+
+float Point::getX(){
+    return this.x;
+}
+
+float Point::getY(){
+    return this.y;
+}
+float Point::distance(Point i){
+    return pow((this.x-i.getX()),2.0)+pow((this.y-i.getY()),2.0)
 }
