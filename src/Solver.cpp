a9e19c7d11bbc1b8b5cf099e86f35fc131041e83
Antonin Gallot <agallot@a-018818.ups.u-psud.fr>
2019-02-13 15:54:17 +0100

    Première version de Solver

--- src/Solver.cpp
+++ src/Solver.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
-#include "Point.h"
+#include "Solver.h"
 
-float min_DP(vector<vector<float> > C, int i, int k){
-        float min = C[0][k-1] + cost_median(1, i);
+using namespace std;
+
+Solver::Solver(Instance p, int N, int K) : pareto(p), matrix(boost::extents[N][K]) {}
+
+
+
+float Solver::min_DP(int i, int k){
+        float min = matrix[0][k-1] + pareto.cost_median(1, i);
         for (int j = 2; j < i; j++){
-            float tmp = C[j-1][k-1] + cost_median(j, i);
+            float tmp = this->matrix[j-1][k-1] + pareto.cost_median(j, i);
             if(tmp < min){
                 min = tmp;
             }
@@ -14,15 +20,15 @@ float min_DP(vector<vector<float> > C, int i, int k){
         return min;
     }
 
-    float DP(vector<vector<float> > compute, int N, int K){
+void Solver::DP(int N, int K){
         vector<vector<float> > C (N, vector<float> (N));
         for (int i = 0; i < N; i++){
-            C[i][1] = compute[1][i];
+            C[i][1] = pareto.cost_median(1, i);
             for (int k = 2; k < K; k++){
-                C[i][k] = min_DP(C, i, k);
+                C[i][k] = min_DP(i, k);
             }
         }
-        return C[N][K];
+        //return C[N][K];
     }
 
     float backtrack(vector<vector<float> > compute, vector<vector<float> > C, int N, int K){
