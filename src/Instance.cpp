b506d8c0197136281a7c04c8589c1862a844e716
Antonin Gallot <agallot@a-018818.ups.u-psud.fr>
2019-02-13 14:42:44 +0100

    Parser fonctionnel + début de Solver

--- src/Instance.cpp
+++ src/Instance.cpp
@@ -12,7 +12,7 @@ class Instance {
     }
 
     float cost_median(int i, int iprime){
-        float min = MAX_FLOAT;
+        float min = 0;
         for(int I = i; I < iprime; I++){
             float sum = 0;
             for (int j = i; j < iprime; j++){
@@ -25,29 +25,5 @@ class Instance {
         return min;
     }
 
-    float min_DP(vector<vector<float> > C, int i, int k){
-        float min = C[0][k-1] + cost_median(1, i);
-        for (int j = 2; j < i; j++){
-            float tmp = C[j-1][k-1] + cost_median(j, i);
-            if(tmp < min){
-                min = tmp;
-            }
-        }
-        return min;
-    }
-
-    float DP(vector<vector<float> > compute, int N, int K){
-        vector<vector<float> > C (N, vector<float> (N));
-        for (int i = 0; i < N; i++){
-            C[i][1] = compute[1][i];
-            for (int k = 2; k < K; k++){
-                C[i][k] = min_DP(C, i, k);
-            }
-        }
-        return C[N][K];
-    }
-
-    float backtrack(vector<vector<float> > compute, vector<vector<float> > C, int N, int K){
-        
-    }
+    
 };
