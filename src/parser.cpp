8c8bc408b3fdcf64991e42e0b2432d78bca901d1
Antonin Gallot <agallot@a-018818.ups.u-psud.fr>
2019-02-13 14:24:14 +0100

    Parser fonctionnel + début de Solver

--- src/parser.cpp
+++ src/parser.cpp
@@ -9,27 +9,25 @@ using namespace std;
 // vector<point> + taille vector + taille données en attributs privés de cette instance
 // matrices et données utilisées dans calculs intermédiaires en attributs privés aussi
 vector<Point> parsing (int n, string filename) {
-    cout << "Test fonc";
-    vector<Point> res (n);
+    cout << "Test fonc\n";
+    vector<Point> res (0);
     float x, y;
     int taille, dimension, cpt = 0;
-    //int cpt = 0;
     ifstream file (filename);
     if (file.is_open()){
         file >> taille;
         file >> dimension;
     }
-    cout << "Taille = " << taille << " Dimension = " << dimension;
+    cout << "Taille = " << taille << " Dimension = " << dimension << "\n";
     if (taille != 0) {
         taille = min(n, taille);
     }
-    while (cpt <= taille){
+    while (cpt < taille){
         if(file.is_open()){
             file >> x;
             file >> y;
             Point p(x, y);
             res.push_back(p);
-            taille++;
             cpt++;
         }
         
@@ -40,7 +38,7 @@ vector<Point> parsing (int n, string filename) {
 
 void display(vector<Point> tab) {
     unsigned int i;
-    for(i = 0; i < tab.size(); i++){
+    for(i = 0; i < tab.size()-1; i++){
         tab[i].display();
     }
 }
@@ -49,8 +47,10 @@ int main (int argc, char *argv[]) {
     vector<Point> tab (20);
     cout << "Test main\n";
     tab = parsing(20, "data.txt");
+    cout << "vector construit\n";
     //vector<Point> tab (10, Point());
     display(tab);
+    cout << tab.size();
     
     return 0;
 }
