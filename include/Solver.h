a24ac9770f5f701ae9be238ab7ad9fbefa27b755
Antonin Gallot <antonin.gallot@a-018818.ups.u-psud.fr>
2019-02-26 12:50:02 +0100

    MAJ

--- include/Solver.h
+++ include/Solver.h
@@ -3,6 +3,7 @@
 #include "Instance.h"
 #include "boost/multi_array.hpp"
 
+// Syntaxe de boost qui permet de créer une matrice de float de dimension 2
 typedef boost::multi_array<float, 2> array_type;
 typedef array_type::index index;
 
@@ -10,11 +11,14 @@ class Solver {
     private:
     Instance pareto;
     array_type matrix;
+    std::vector<int> solution;
 
 
     public:
     Solver(Instance &p, int N, int K);
     float min_DP(int i, int k);
     void DP(int N, int K);
-    float backtrack(int N, int K);
+    void backtrack(int N, int K);
+    std::vector<int> getSolution();
+    void fillArray();
 };
