e5944a66d3f12c04d0cb95e9f8c318d7346bba70
Adrien Chesneau <achesne@a-018818.ups.u-psud.fr>
2019-02-27 17:16:31 +0100

    Opti

--- include/Instance.h
+++ include/Instance.h
@@ -1,20 +1,33 @@
 #include "Point.h"
+#include "boost/multi_array.hpp"
 #include <vector>
+
+typedef boost::multi_array<float, 2> array_type;
+typedef array_type::index index;
 
 class Instance {
     private:
     std::vector<Point> pareto;
     int N;
     int K;
+    array_type matrix;
+
 
     public:
     Instance();
     Instance(std::vector<Point> v);
     Point getPoint(int i);
+//Calcul avec matrice    
     float cost_median(int i, int iprime);
     float cost_means(int i, int iprime);
     float cost_medoids(int i, int iprime);
     float cost_dcenter(int i, int iprime);
     float cost_dcenterv2(int i, int iprime);
     float cost_ccenter(int i, int iprime);
+//Calcul sans matrice    
+    float old_cost_median(int i, int iprime);
+    float old_cost_medoids(int i, int iprime);
+    float old_cost_dcenter(int i, int iprime);
+    float old_cost_dcenterv2(int i, int iprime);
+    float old_cost_ccenter(int i, int iprime);
 };
--- src/Instance.cpp
+++ src/Instance.cpp
@@ -8,47 +8,59 @@ using namespace std;
 
 Instance::Instance(){}
 
-Instance::Instance(std::vector<Point> v): pareto(v){ }
+Instance::Instance(std::vector<Point> v): pareto(v), N(v.size){ 
+    for(int i=0; i<N; i++){
+        matrix[i][i+1] = this->getPoint(i).distance(this->getPoint(i+1));
+    }
+    for(int i=0; i<N; i++){
+        for (int j=i+2; j<N; j++){
+            matrix[i][j]= matrix[i][j-1]+matrix[j-1][j];
+        }
+    }
+}
 
 Point Instance::getPoint(int i){ return pareto[i];}
 
+//Calcul avec matrice (à faire)
 
 float Instance::cost_means(int i, int iprime){
     float sum = 0;
     int cpt = 0;
     Point p(0,0);
-    for(int j= i; j < iprime; j++){
+    for(int j= i; j <= iprime; j++){
         p.addPoint(getPoint(j));
     }
     p.multInt(1/(iprime-i+1));
-    for(int I = i; I < iprime; I++){
+    for(int I = i; I <= iprime; I++){
         sum+= this->getPoint(I).distance(p);
     }
-    sum = (1/(iprime-i+1))* sum;
     return sum;
 }
 
 float Instance::cost_medoids(int i, int iprime){
     float min = FLT_MAX;
-    for(int I = i; I < iprime; I++){
+    if(i == iprime) {min = 0;}
+    for(int I = i; I <= iprime; I++){
         float sum = 0;
-        for (int j = i; j < iprime; j++){
-            sum += this->getPoint(I).distance(this->getPoint(j));
+        for (int j = i; j <= iprime; j++){
+            if(I<j){sum += matrix[I][j];}
+            if(j<I){sum += matrix[j][I];}
         }
         if (sum < min){
             min = sum;
         }
     }
-    min = (1/(iprime-i+1))* min;
     return min;
 }
 
 float Instance::cost_median(int i, int iprime){
     float min = FLT_MAX;
-    for(int I = i; I < iprime; I++){
+    if(i == iprime) {min = 0;}
+    for(int I = i; I <= iprime; I++){
         float sum = 0;
-        for (int j = i; j < iprime; j++){
-            sum += this->getPoint(I).distance(this->getPoint(j));
+        for (int j = i; j <= iprime; j++){
+            if(I<j){sum += matrix[I][j];}
+            if(j<I){sum += matrix[j][I];}
         }
         sum = sqrt(sum);
         if (sum < min){
@@ -60,7 +72,88 @@ float Instance::cost_median(int i, int iprime){
     
 float Instance::cost_dcenter(int i, int iprime){
     float min = FLT_MAX;
-    for(int j=i; j < iprime; j++){
+    if(i == iprime) {min = 0;}
+    for(int j=i; j <= iprime; j++){
+        float sum = 0;
+        if(matrix[i][j]>matrix[j][iprime]){
+            sum += matrix[i][j];
+        }
+        else {
+            sum += matrix[j][iprime];
+        }
+        sum = sqrt(sum);
+        if (sum < min){
+            min = sum;
+        }
+    }
+    return min;
+}
+
+float Instance::cost_dcenterv2(int i, int iprime){
+    float min = FLT_MAX;
+    int j = i;
+    while(j< iprime){
+        float sum = 0;
+        if(matrix[i][j]>matrix[j][iprime]){
+            sum += matrix[i][j];
+        }
+        else {
+            sum += matrix[j][iprime];
+        }
+        sum = sqrt(sum);
+        if (sum < min){
+            min = sum;
+            i += 1;
+        }
+        else{
+            return min;
+        }
+    }
+    return min;
+}
+
+float Instance::cost_ccenter(int i, int iprime){
+    float sum = matrix[i][iprime];
+    return 0.5*sqrt(sum);
+}
+
+//Calcul sans distance
+
+float Instance::old_cost_medoids(int i, int iprime){
+    float min = FLT_MAX;
+    if(i == iprime) {min = 0;}
+    for(int I = i; I <= iprime; I++){
+        float sum = 0;
+        for (int j = i; j <= iprime; j++){
+            sum += this->getPoint(I).distance(this->getPoint(j));
+        }
+        if (sum < min){
+            min = sum;
+        }
+    }
+    return min;
+}
+
+float Instance::old_cost_median(int i, int iprime){
+    float min = FLT_MAX;
+    if(i == iprime) {min = 0;}
+    for(int I = i; I <= iprime; I++){
+        float sum = 0;
+        for (int j = i; j <= iprime; j++){
+            sum += this->getPoint(I).distance(this->getPoint(j));
+        }
+        sum = sqrt(sum);
+        if (sum < min){
+            min = sum;
+        }
+    }
+    return min;
+}
+    
+float Instance::old_cost_dcenter(int i, int iprime){
+    float min = FLT_MAX;
+    if(i == iprime) {min = 0;}
+    for(int j=i; j <= iprime; j++){
         float sum = 0;
         if(this->getPoint(j).distance(this->getPoint(i))>this->getPoint(j).distance(this->getPoint(iprime))){
             sum = this->getPoint(j).distance(this->getPoint(i));
@@ -76,7 +169,7 @@ float Instance::cost_dcenter(int i, int iprime){
     return min;
 }
 
-float Instance::cost_dcenterv2(int i, int iprime){
+float Instance::old_cost_dcenterv2(int i, int iprime){
     float min = FLT_MAX;
     int j = i;
     while(j< iprime){
@@ -99,6 +192,7 @@ float Instance::cost_dcenterv2(int i, int iprime){
     return min;
 }
 
-float Instance::cost_ccenter(int i, int iprime){
-    return 0.5*sqrt(this->getPoint(i).distance(this->getPoint(iprime)));
+float Instance::old_cost_ccenter(int i, int iprime){
+    float sum = this->getPoint(i).distance(this->getPoint(iprime));
+    return 0.5*sqrt(sum);
 }
